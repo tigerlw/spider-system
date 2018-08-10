053247e1ddba8d8b29b52dfbe41d600a2f38a9ea
tigerlw <301076651@qq.com>
2018-08-10 16:01:31 +0800

    'modify'

--- src/com/ExceptionMain.py
+++ src/com/ExceptionMain.py
@@ -5,7 +5,10 @@ client = Client('https://2daca688baa548119402d9d8639f941f@sentry.io/1258373')
 
 try:
     result = "123" + "1"
-    result = "123" + 4
-except Exception, e:
+    #result = "123" + 4
+    msg = traceback.format_exc(1)
+    print(msg)
+except Exception as e:
     traceback.print_exc()
-    client.captureException()
+    # client.captureException()
+    # print(e)
--- src/com/LoggerMain.py
+++ src/com/LoggerMain.py
@@ -3,10 +3,10 @@ import logging
 logger = logging.getLogger('mylogger')
 logger.setLevel(logging.DEBUG)
 
-fh = logging.FileHandler('test.log')
+fh = logging.FileHandler('../../../logs/test.log')
 fh.setLevel(logging.DEBUG)
 
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(filename)s - %(funcName)s - %(lineno)s - %(message)s')
 fh.setFormatter(formatter)
 
 logger.addHandler(fh)
